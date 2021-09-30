e71dda90dfd7aeb94581a719b5ca4d3aa6472230
Matias <3959@holbertonschool.com>
2021-09-30 10:48:13 -0700

    s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %lu bytes\n", sizeof(charType));
 printf("Size of an int: %lu bytes\n", sizeof(intype));
 printf("Size of a long int: %lu bytes\n", sizeof(longinType));
 printf("Size of a long long int: %lu bytes\n", sizeof(longlongType));
-prinft("Size of a float: %lu bytes\n", sizeof(floatType));
+printf("Size of a float: %lu bytes\n", sizeof(floatType));
 return (0);
 }
