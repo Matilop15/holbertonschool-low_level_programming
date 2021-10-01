dedbe187dae6565cdcf7afbc95313e2a5fec3e0c
Matias <3959@holbertonschool.com>
2021-10-01 12:13:38 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - imprimir el abecedario
+* Return: is 0!!!
+*/
+int main(void)
+{
+	char alf;
+
+	for (alf = '0' ; alf <= '9' ; alf++)
+		putchar (alf);
+		putchar ('\n');
+	return (0);
+}
