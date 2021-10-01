a0bc2327e600dac9a570fc935a7ca92e4e61f279
Matias <3959@holbertonschool.com>
2021-10-01 11:14:04 -0700

    alfa

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - imprimir el abecedario
+* Return: is 0!!!
+*/
+int main(void)
+{
+	char alf;
+
+	for (alf = 'a' ; alf <= 'z' ; alf++)
+	putchar (c);
+	return (0);
+}
