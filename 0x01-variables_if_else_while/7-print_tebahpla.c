4748769671922e57d402b5d5b383c0343e14792d
Matias <3959@holbertonschool.com>
2021-10-01 12:35:27 -0700

    z...a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - imprimir el abecedario
+* Return: is 0!!!
+*/
+int main(void)
+{
+	char alf;
+
+	for (alf = 'z' ; alf <= 'a' ; alf--)
+		putchar (alf);
+		putchar ('\n');
+	return (0);
+}
