080ad9d624faa33805de278053534658232fffb2
Matias <3959@holbertonschool.com>
2021-10-05 10:24:08 -0700

    signed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,21 +6,17 @@
 */
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
 	_putchar('+');
 		return (1);
 	}
-		if (n == '0')
+		if (n == 0)
 		{
 			_putchar('0');
 			return (0);
 		}
-		else
-		(n < '0')
-			{
-				_putchar('-');
-				return (-1);
-			}
-				return (2);
+
+	_putchar('-');
+	return (-1);
 }
