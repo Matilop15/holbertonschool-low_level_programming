ebe4d693266d99deb4f6da4d8ef4058afbbd234a
Matias <3959@holbertonschool.com>
2021-10-05 11:01:56 -0700

    absolutenumber

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,18 @@
 #include "main.h"
 /**
-* _abs - absolute numbers
+*_abs - absolute numbers
 *@mat: variable
 *Return: 0 esta todo bine
 */
-
 int _abs(int mat)
 {
 	if (mat < 0)
 	{
-		_putchar(mat * -1);
+	return (mat * -1);
 	}
 		if (mat > 0)
 		{
-			_putchar(mat);
+			return (mat);
 		}
 		return (0);
 }
-
-
