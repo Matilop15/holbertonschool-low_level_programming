d9a8fd89ab6d40a908996899eef140a728e3e300
Matias <3959@holbertonschool.com>
2021-10-05 08:00:29 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - write alphabet
+* Return: no exist
+*/
+
+void print_alphabet(void)
+{
+	int alf;
+
+	for (alf = 'a' ; alf <= 'z' ; alf++)
+	{
+		_putchar(alf);
+	}
+	_putchar('\n');
+}
