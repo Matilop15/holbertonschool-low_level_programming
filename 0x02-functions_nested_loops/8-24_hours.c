4e027e85e356658f704e54ec6fffbc3c6e3c9626
Matias <3959@holbertonschool.com>
2021-10-05 16:39:56 -0700

    24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+  *jack_bauer - no idea que es
+  *
+  * no hay return
+  */
+
+void jack_bauer(void)
+{
+	int hour, h, min, seg;
+
+	for (hour = 0 ; hour <= 2 ; hour++)
+	{
+		for (h = 0 ; h <= 9 ; h++)
+		{
+			for (min = 0 ; min <= 5 ; min++)
+			{
+				for (seg = 0 ; seg <= 9 ; seg++)
+				{
+					if (hour == 2 && h > 3)
+					{
+						break;
+					}
+					_putchar('0' + hour);
+					_putchar('0' + h);
+					_putchar(':');
+					_putchar('0' + min);
+					_putchar('0' + seg);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
