e8483c7ee530e749b4809d352feaf2da724eac71
Matias <3959@holbertonschool.com>
2021-10-05 09:50:39 -0700

    possitive and negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - dependiendo si es negativo o no
+*@n: aleatory number
+* Return: 1 and prints + if n is greater than zero, -1 and prints -
+*/
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+	_putchar('+');
+		return (1);
+	}
+		if (n == '0')
+		{
+			_putchar('0');
+			return (0);
+		}
+			if (n < '0')
+			{
+				_putchar('-');
+				return (-1);
+			}
+			return (2);
+}
