41b7c53786ecc12f58946568f68609935a0872be
Matias <3959@holbertonschool.com>
2021-10-05 19:46:24 -0700

    tabla grande

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,43 @@
 #include "main.h"
 /**
-  *
-  *
+  *print_times_table - print tabla grande
+  *@n: variable calculad
   */
 void print_times_table(int n)
 {
 	int fila, columna, resultado;
 
-	if (n <= 15 && n >= 0)
+	if (n < 15 && n >= 0)
 	{
 		for (fila = 0 ; fila <= n ; fila++)
 		{
+			_putchar('0')
 			for (columna = 0 ; columna <= n ; columna++)
 			{
 				resultado = fila * columna;
-				if (columna != 0)
+				if (resultado <= 9)
 				{
-					_putchar(',');
+					_putchar('5');
 					_putchar(' ');
-				}
-				if (fila == 0 && columna != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				if (resultado != 0 && resultado < 10)
-				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(resultado + '0');
-				} else if (resultado >= 10 && resultado < 100)
-				{
-					_putchar(' ');
-					_putchar((resultado / 10) + '0');
-					_putchar((resultado % 10) + '0');
 				}
-				 else if (resultado >= 100 && resultado < 1000)
+				else if (resultado < 100)
 				{
-					_putchar(((resultado / 100) % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(resultado / 10 + '0');
+					_putchar(resultado % 10 + '0');
+				}
+				else
+				{	
+					_putchar(',');
+					_putchar(' ');
+					_putchar(((resultado / 10) / 10) + '0');
 					_putchar(((resultado / 10) % 10) + '0');
-					_putchar((resultado % 10) + '0');
-				} else
-				{
-				_putchar(resultado + '0');
+					_putchar(resultado % 10 + '0');
 				}
 			}
 			_putchar('\n');
