d4b432984894de66e42a213cc6efc44e3efb5f10
Matias <3959@holbertonschool.com>
2021-10-05 17:03:32 -0700

    cuentarar

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+  *
+  *
+  *
+  *
+ */
+void times_table(void)
+{
+	int fila, columna, resultado;
+
+	for (fila = 0 ; fila <= 9 ; fila++)
+	{
+		for (columna = 0 ; columna <= 9 ; columna++)
+		{
+			resultado = fila * columna;
+			if (columna == 0)
+			{
+				_putchar('0' + resultado);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+				if (resultado / 10 == 0)
+					{
+					_putchar(' ');
+					_putchar('0' + resultado);
+					}
+				else
+				{
+					_putchar('0' + resultado / 10);
+					_putchar('0' + resultado % 10);
+				}
+		}
+	_putchar('\n');	
+	}
+}
