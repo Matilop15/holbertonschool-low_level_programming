9efb9580f9c5825b9eb3af4f49336a64dcc5cf68
Matias <3959@holbertonschool.com>
2021-10-07 17:44:23 -0700

    fiizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+  *main - print num 1-100 and fizzbuzz in multipl of 15
+ *Return: if 0 all ok
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 5 == 0) && (num % 3 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", num);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
