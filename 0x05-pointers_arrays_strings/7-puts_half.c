e93716018b4c5658cc380dd2390fef36e3140f7f
Matias <3959@holbertonschool.com>
2021-10-12 18:44:31 -0700

    lasegmitad

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *puts_half - la segunda mitad de str
+  *@str: punterooo
+  */
+void puts_half(char *str)
+{
+	int a, b = 0;
+
+	for (a = 0; str[a] != 0; a++)
+	{
+	}
+	for (b = 0; str[b] != 0; b++)
+	{
+		if (b >= (a / 2))
+		{
+			_putchar(str[b]);
+		}
+	}
+		_putchar('\n');
+}
