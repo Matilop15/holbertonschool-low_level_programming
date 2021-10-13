52a0e3fd872c11c97d103785ef38341eeefe0244
Matias <3959@holbertonschool.com>
2021-10-12 18:11:02 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,13 @@ void puts2(char *str)
 {
 	int a;
 
-	for (a = 0; str[a] != 0; a += 2)
+	for (a = 0; str[a] != '\0'; a++)
 	{
-		_putchar(str[a]);
+		if ((a % 2) == 0)
+		{
+			_putchar(str[a]);
+		}
+		
 		if (*str == '\0')
 		{
 			break;
