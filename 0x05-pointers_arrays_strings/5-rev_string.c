26b7c69c5659d9fd14d58d90a79c4450611798ee
Matias <3959@holbertonschool.com>
2021-10-13 19:30:26 -0700

    reversee

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 /**
   *rev_string - reverse of string
   *@s: string
   */
 void rev_string(char *s)
 {
-	int a = 0;
+	int tam, last;
+	char buf;
 
-	for (
+	for (tam = 0; s[tam] != 0; tam++)
+	{
+	}
+	tam = tam - 1;
+	for (last = tam; last >= 0; last--)
+	{
+		if (last == tam / 2)
+		{
+			break;
+		}
+		else
+		{
+		buf = *(s + (tam - last));
+		*(s + (tam - last)) = *(s + last);
+		*(s + last) = buf;
+		}
+	}
 }
