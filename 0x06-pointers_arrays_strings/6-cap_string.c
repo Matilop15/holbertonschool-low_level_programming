b749a2073baba2c202774f15a487cb7f702d783f
Matias <3959@holbertonschool.com>
2021-10-15 15:45:26 -0700

    uppercaseandotodo

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+  *cap_string - cambiar las minusculas desp de un simbolo
+  *@a: texto a modificar
+ *@c: simbolos a comparar
+  *Return: txto dado
+  */
+char *cap_string(char *a)
+{
+	int b, d;
+	int c[] = {'.', ',', '(', ')', '{', '}', '!', '?', '"', 10, '-', 9, ' '};
+
+	for (b = 0; a[b] != 0; b++)
+	{
+		if (a[0] >= 97 && a[b]<= 122)
+		{
+			a[b] = a[b] - 32;
+		}
+
+		for (d = 0; c[d] != 0; d++)
+		{
+
+		if (a[b] == c[d])
+		{
+			b++;
+
+			if (a[b] >= 97 && a[b] <= 122)
+			{
+				a[b] = a[b] - 32;
+			}
+		}
+		}
+	}
+	return (a);
+}
