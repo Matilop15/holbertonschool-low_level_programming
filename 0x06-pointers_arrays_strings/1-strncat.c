9d1574076893db924a2bb3befb9e5fd29e2ba7a3
Matias <3959@holbertonschool.com>
2021-10-15 09:22:59 -0700

    reupload 1 for check 4 and 5

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
-  *
-  *
-  *
+  *_strncat - copy n cantidad de characters
+  *@dest: string finnally
+  *@src: origin string
+  *@n: cantidad de lugares a moverse
+  *Return: devuelve string finally
   */
 char *_strncat(char *dest, char *src, int n)
 {
 	int a, b;
-	
+
 	for (a = 0; dest[a] != 0; a++)
 	{
 	}
-	for (b = 0; (src[b] != 0 && b < n); b++)
+		for (b = 0; (src[b] != 0 && b < n); b++)
 		{
 			dest[a] = src[b];
 			a++;
