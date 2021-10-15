d00087ec51dc456ac05b1de5c951cbbdfed6a2b2
Matias <3959@holbertonschool.com>
2021-10-15 09:21:40 -0700

    reupload of 0 for check five

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,16 +3,16 @@
   *_strcat - une dos strings
   *@src: string inicial
   *@dest: string final
- * Return: retorna string final 
-*/
+ *Return: retorna string final
+ */
 char *_strcat(char *dest, char *src)
 {
 	int a, b;
-	
+
 	for (a = 0; dest[a] != 0; a++)
 	{
 	}
-	for (b = 0; src[b] != 0; b++)
+		for (b = 0; src[b] != 0; b++)
 		{
 			dest[a] = src[b];
 			a++;
