81e83e7e15c295e1e5499e77e12683abc64155ef
Matias <3959@holbertonschool.com>
2021-10-18 19:20:43 -0700

    agregandoputchar

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_chessboard - print a chees board :D
+  *@a: matris a seguir
+  */
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+		_putchar(10);
+	}
+}
