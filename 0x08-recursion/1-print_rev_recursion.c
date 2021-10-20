d5bce243d35f897d21582070106bc4663336ddd2
Matias <3959@holbertonschool.com>
2021-10-20 07:17:32 -0700

    printreverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *_print_rev_recursion - function to use for print this string
+  *@s: string for print
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+		s++;
+	}
+}
