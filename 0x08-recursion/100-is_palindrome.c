9480cdd56a8893b75b2f54a5f78882af52ab1995
Matias <3959@holbertonschool.com>
2021-10-20 19:10:24 -0700

    as

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,16 +7,6 @@
 
 int is_palindrome(char *s)
 {
-	return (0);
-}
-/**
-*_print_rev_recursion - da vuelta
-*@s: string for print
-*Return: 0 si el string tiene largo 0, sino el string al reves
-*/
-
-int _print_rev_recursion(char *s)
-{
 	int rec = 0;
 
 	if (*s == '\0')
