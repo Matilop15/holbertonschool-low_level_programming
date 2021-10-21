964c0d4a44106d295dcdd3e9f5e1cde68e3ff968
Matias <3959@holbertonschool.com>
2021-10-20 19:18:37 -0700

    probando2

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *wildcmp - toma todo como igual
+  *@s1: no idea
+  *@s2: no idea2
+  *Return: 0 porque se me canto jaj
+  */
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 != s2)
+	{
+		return (1);
+	}
+	return (0);
+}
