bcfad34deb9ba022a9e89e6bde082cac8da37217
Matias <3959@holbertonschool.com>
2021-10-22 06:29:39 -0700

    argcvc1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - print program name
+ *@argc: almacena los
+ *@argv: tinee la cadena
+ *Return: return cerroo
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
+
