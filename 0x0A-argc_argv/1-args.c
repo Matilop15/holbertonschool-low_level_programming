3178bdef75217daa2366f317859b097edb1272c1
Matias <3959@holbertonschool.com>
2021-10-22 06:49:39 -0700

    contandolong

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints the number of arguments
+ *@argc: number of arguments
+ *@argv: content of variable
+ *Return: is ceroo
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)*argv;
+
+	return (0);
+}
