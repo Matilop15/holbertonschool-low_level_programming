fee8044cd6512e0fce5cd0aa79a3d4105185fb21
Matias <3959@holbertonschool.com>
2021-10-22 18:40:23 -0700

    changeprot4

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - minimum number of coins to make chang
+ *@argc: long of array
+ *@argv: input number
+ *Return: return 1 if not is only 1 number
+ */
+int main(int argc, char *argv[])
+{
+	int coin;
+	int a = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	coin = atoi(argv[1]);
+
+	if (a < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (a = 0; coin > 0; a++)
+	{
+		if (coin >= 25)
+		{
+			coin = coin - 25;
+		}
+		else if (coin >= 10)
+		{
+			coin = coin - 10;
+		}
+		else if (coin >= 5)
+		{
+			coin = coin - 5;
+		}
+		else if (coin >= 2)
+		{
+			coin = coin - 2;
+		}
+		else if (coin >= 1)
+		{
+			coin = coin - 1;
+		}
+	}
+	printf("%d\n", a);
+	return (0);
+}
