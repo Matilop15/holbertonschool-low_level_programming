868ea1b361f61f3e57516599c99d399a4a2a07bd
Matias <3959@holbertonschool.com>
2021-10-23 08:02:42 -0700

    reuppload4

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,50 @@
-int _atoi(char *s);
+#include "main.h"
+/* Task 100 0x05. C - Pointers, arrays and strings  */
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: String to convert
+ * Description: convert a string to an integer.
+ * Return: void
+ */
+int _atoi(char *s)
+{
+	int i;
+	int j;
+	int l;
+	unsigned int a;
+	int cont;
+
+	i = 0;
+	l = 0;
+	while (s[i] != '\0')
+	{
+		l++;
+		i++;
+	}
+
+	cont = 0;
+	a = 0;
+
+	for (j = 0 + cont; j <= l - 1; j++)
+	{
+		if (s[j] >= 48 && s[j] <= 57)
+		{
+			a = ((a * 10) + s[j] - '0');
+			if (s[j + 1] < 48 || s[j + 1] > 57)
+				break;
+		}
+	}
+
+	cont = 0;
+	j = 0;
+	while (cont != 1)
+	{
+		if (s[j] >= 48 && s[j] <= 57)
+			cont = 1;
+		if (s[j] == 45)
+			a = (a * -1);
+		j++;
+	}
+	return (a);
+}
