1bf49cc47ec2a7af5c8a2340a5359165f7fd1afd
Matias <3959@holbertonschool.com>
2021-10-26 06:59:21 -0700

    probandomalloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *create_array - array c
+ *size: of array c
+ *c: variable
+ *Return: if c is 0, null. if c is a number, this number-
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a;
+	char *b;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	b = malloc(size * sizeof(char));
+	for (a = 0; size == a; a++)
+	{
+		b[a] = c;
+	}
+
+	return (b);
+}
