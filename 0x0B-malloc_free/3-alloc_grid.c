382d0d264c0378d1095894a25bce5d96a01166a7
Matias <3959@holbertonschool.com>
2021-10-26 16:18:25 -0700

    array2dp3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *alloc_grid - puntero a puntero
+ *@width: ancho del array 2d/size of column
+ *@height: alto del array 2d/size of rows
+ *Return: null if wodth or heiht is 0, else return array 2d
+ */
+int **alloc_grid(int width, int height)
+{
+	int a, b;
+	int **dest;
+
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	dest = (int **)malloc(sizeof(int *) * height);
+	if (dest == NULL)
+	{
+		free(dest);
+		return (NULL);
+	}
+	for (a = 0; a < height; a++)
+	{
+		dest[a] = (int *)malloc((sizeof(int *) * width));
+		if (dest[a] == NULL)
+		{
+			for (a--; a >= 0; a--)
+			{
+				free(dest[a]);
+			}
+			free(dest);
+			return (NULL);
+
+		}
+		for (b = 0; b < width; b++)
+		{
+			dest[a][b] = 0;
+		}
+	}
+	return (dest);
+}
