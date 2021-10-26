7526baec745f105b74597b3fcb4beb3114b3e3cf
Matias <3959@holbertonschool.com>
2021-10-26 09:41:48 -0700

    duplicateandpaste1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup - copia el largo de una cadena
+ *@str: string to copy
+ *Return: if size str es 0, NULL. if size of copy str is 0, NULL
+ */
+char *_strdup(char *str)
+{
+	char *dest;
+	int a = 1, b;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (; str[a] != 0; a++)
+	{
+	}
+	dest = malloc((a * sizeof(str)) + 1);
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	for (b = 0; b < 10; b++)
+	{
+		dest[b] = str[b];
+	}
+	return (dest);
+}
