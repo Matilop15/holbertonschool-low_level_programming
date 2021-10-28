18ec258589fde7b8eaec590db28f3058f78ce4e3
Matias <3959@holbertonschool.com>
2021-10-28 10:39:24 -0700

    prubeasin return0

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (c = 0; c <= a; c++)
+	for (c = 0; c < a; c++)
 	{
 		dest1[c] = s1[c];
 	}
@@ -45,5 +45,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		dest1[c] = s2[a];
 		a++;
 	}
+	c++;
+	dest1[c] = '\0';
 	return (dest1);
 }
