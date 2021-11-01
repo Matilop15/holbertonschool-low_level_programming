552c701e386b8e2c0c6dfd003574028949b071f2
Matias <3959@holbertonschool.com>
2021-11-01 09:57:08 -0700

    doogg

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ *dog - struct dog
+ *@name: charrr
+ *@age: floaattt
+ *@owner: charrrr
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
