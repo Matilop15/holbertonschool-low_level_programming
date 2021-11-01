2a5d5e130c91eb2d67d83d2e39cf08e41e0d59a5
Matias <3959@holbertonschool.com>
2021-11-01 11:58:41 -0700

    typedefdof:T

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,11 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ *dog_t - typedef for dog type estructure
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
