774d5209758a847fb35705c8c7cbe89ededec5b7
Matias <3959@holbertonschool.com>
2021-11-01 07:24:06 -0700

    task2and3 :D

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - print name of file
+*Return: cerroo
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef HEADER
+#define HEADER
+#define ABS(x) ( (x > 0) ? num : (num * -1) )
+#endif
