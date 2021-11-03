846a500ddb2d10066580804c2f0ece459470a119
Matias <3959@holbertonschool.com>
2021-11-03 08:20:42 -0700

    returnnoting

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 /**
  *print_name - print name of
  *@name: text to print
  *@f: pointer to fuctionc char
- */
+*Return: nothing
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
