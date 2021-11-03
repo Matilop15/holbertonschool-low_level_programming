c4f40dcce73662498fceb9ea78e59196f7e01647
Matias <3959@holbertonschool.com>
2021-11-03 06:14:44 -0700

    uppload task 0, REadme and fuction.h

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ *print_name - print name of
+ *@name: text to print
+ *@f: pointer to fuctionc char
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != 0 && f != 0)
+	{
+		f(name);
+	}
+}
