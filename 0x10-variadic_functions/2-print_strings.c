9c45cce95450e828bd6416699d0940333bc0baa4
Matias <3959@holbertonschool.com>
2021-11-04 11:01:30 -0700

    sadas

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("(nil)");
 			else
 				printf("%s", dest);
-			if (n - 1 > a && separator == NULL)
+			if (n - 1 != a && separator != NULL)
 				printf("%s", separator);
 		}
 	}
