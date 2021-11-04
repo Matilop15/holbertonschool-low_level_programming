c08d455b2557b93cea1710827dbbc57adc92cf45
Matias <3959@holbertonschool.com>
2021-11-03 19:13:17 -0700

    change get

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,10 +20,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (ops[i].op[0] == s[0] && ops[i].op != NULL)
+		if (s && s[0] == ops[i].op[0] && !s[1])
 			return (ops[i].f);
 		i++;
 	}
-	printf("Error\n");
-	exit(99);
+	return (NULL);
 }
