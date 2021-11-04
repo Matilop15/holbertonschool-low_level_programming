63872097250a67d31229f241b4afb5694fd911bd
Matias <3959@holbertonschool.com>
2021-11-03 17:09:20 -0700

    verificando todo

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,11 +2,6 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_div(int a, int b);
 /**
  *op_add - sum a and b
  *@a: number a
