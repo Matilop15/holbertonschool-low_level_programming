90777bc6eb735f79ed30e690410c08f9ceb277eb
Matias <3959@holbertonschool.com>
2021-11-04 06:44:20 -0700

    print con separtorrr

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ *print_numbers - print numbers to array
+ *@separator: character separtor
+ *@n: number of characters
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list param;
+	unsigned int a;
+	int s;
+
+	va_start(param, n);
+	s = 0;
+	for (a = 0; a < n; a++)
+	{
+		s = va_arg(param, int);
+		printf("%d", s);
+		if (n - 1 > a)
+		printf("%s", separator);
+	}
+	va_end(param);
+	printf("\n");
+}
