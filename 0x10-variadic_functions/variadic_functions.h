f719fe28ab6251287cb2cb219ca3db85f887a858
Matias <3959@holbertonschool.com>
2021-11-05 05:36:16 -0700

    asds

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,19 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
+
 #include <stdio.h>
 #include <stdarg.h>
-
+/**
+ *struct op - Struct op
+ *@vari: holaa
+ *@f: The function associated
+ */
 typedef struct op
 {
 	char vari;
 	void (*f)(va_list);
 } op_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
