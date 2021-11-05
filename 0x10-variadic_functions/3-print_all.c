71d4ebf5ce8470ebb60bf3e059528e3d3f691667
Matias <3959@holbertonschool.com>
2021-11-04 19:22:35 -0700

    probando task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,78 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *print_char - prints char
+ *@param: arg to print
+ */
+void print_char(va_list param)
+{
+	printf("%c", va_arg(param, int));
+}
+/**
+ *print_int - print integer
+ *@param: arg to printt
+ */
+void print_int(va_list param)
+{
+	printf("%d", va_arg(param, int));
+}
+/**
+ *print_float - prints a float
+ *@param: arg to float
+ */
+void print_float(va_list param)
+{
+	printf("%f", va_arg(param, double));
+}
+/**
+ *print_pchar - prints string
+ *@param: string to print
+ */
+void print_pchar(va_list param)
+{
+	char *point = va_arg(param, char*);
+
+	if (point == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", point);
+}
+/**
+ *print_all - print anything
+ *@format: list types of argument
+ */
+void print_all(const char * const format, ...)
+{
+	va_list param;
+	int a = 0;
+	int b = 0;
+	char *pooint = "";
+	op_t type[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_pchar},
+		{'\0', NULL},
+	};
+
+	va_start(param, format);
+	while (format && format[a])
+	{
+		while (type[b].vari)
+		{
+			if (format[a] == type[b].vari)
+			{
+				printf("%s", pooint);
+				type[b].f(param);
+				pooint = ", ";
+			}
+			b++;
+		}
+		a++;
+	}
+	va_end(param);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,16 +1,14 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef VARIADIC_FUNCTIONS_H_
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
-
+typedef struct op
+{
+	char vari;
+	void (*f)(va_list);
+} op_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-typedef struct op
-{
-	    char var;
-	  void (*f)(va_list);
-} op;
 
 #endif
