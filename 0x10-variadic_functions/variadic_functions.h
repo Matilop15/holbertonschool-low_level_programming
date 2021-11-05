0d14dc8bd1689f1ae51bf8f1e23258fb65cccb66
Matias <3959@holbertonschool.com>
2021-11-04 17:39:54 -0700

    variablee

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef _HEADER_
 #define _HEADER_
-#include <stdio.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
