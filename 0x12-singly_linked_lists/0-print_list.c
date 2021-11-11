990e9c9b888b514c948a424c7a93941a1d346dcd
Matias <3959@holbertonschool.com>
2021-11-11 05:10:32 -0800

    comenzando con linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ *print_list - print number of elements in list_t
+ *
+ *@h: pointer to list
+ *Return: size of linked list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+	while (h->next);
+
+
+
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+##LINKED LIST
+
+#General
+-When and why using linked lists vs arrays
+-How to build and use linked lists
