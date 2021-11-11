d25d2f20b839e7886ab770e257032360e948c8f8
Matias <3959@holbertonschool.com>
2021-11-11 07:31:53 -0800

    task0 prueba 3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->next == NULL)
-			printf("(nil)\n");
-		printf("[%u] %s\n", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		size++;
 	}
