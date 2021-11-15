c818af325757f4d0d13dd2c1a8226cc130880f83
Matias <3959@holbertonschool.com>
2021-11-15 08:26:34 -0800

    task 1 probando

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
+ *print_listint - print number of elements in listint_t
  *
- *
- *
+ *@h: pointer to list
+ *Return: size of linked list
  */
 size_t print_listint(const listint_t *h)
 {
