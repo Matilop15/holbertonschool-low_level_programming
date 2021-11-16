fed55eda826f1a5be0562235cb2ab06c60881a01
Matias <3959@holbertonschool.com>
2021-11-16 06:14:12 -0800

    add task 9 with betty ok

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ *insert_nodeint_at_index - new node at a given position
+ *@head: puntero al puntero
+ *@idx: position to new node
+ *@n: content of the new node
+ *Return: address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode, *temp;
+	unsigned int a = 0;
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	temp = *head;
+	while (a < idx - 1)
+	{
+		temp = temp->next;
+		a++;
+	}
+	newNode->n = n;
+	newNode->next = temp->next;
+	temp->next = newNode;
+	return (newNode);
+}
