fd48f2836da5432ba66256ee6d1e35dabd38c4fd
Matias <3959@holbertonschool.com>
2021-11-16 06:26:43 -0800

    add task 9 with betty ok

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	temp = *head;
-	while (a < idx - 1)
+	while (a < idx)
 	{
 		temp = temp->next;
 		a++;
