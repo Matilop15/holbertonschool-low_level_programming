d170a049f8d6b46aa3221b2316e5dddf6f5b816f
Matias <3959@holbertonschool.com>
2021-11-16 05:22:54 -0800

    agregando exit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *buffer;
 
-	if (*head == NULL)
-		return;
 	while (*head)
 	{
+		if (*head == NULL)
+			return;
 		buffer = ((*head)->next);
 		free(*head);
 		*head = buffer;
