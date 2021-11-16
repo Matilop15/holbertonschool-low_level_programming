a3802300b85b3c62e5d75d18e5f918718063b711
Matias <3959@holbertonschool.com>
2021-11-16 04:57:26 -0800

    arreglando el 5, sacando ()

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,9 @@ void free_listint2(listint_t **head)
 
 	while (head)
 	{
-		buffer = *head;
-		*head = buffer->next;
-		free(buffer);
+		buffer = ((*head)->next);
+		free(*head);
+		*head = buffer;
 	}
 	*head = NULL;
 }
