8f08d5a2c642c84eca88d19f63e2c294ba3f63db
Matias <3959@holbertonschool.com>
2021-11-16 05:55:47 -0800

    add index =0

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,10 +9,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int a = 0;
 
-	if (head == NULL)
-		return (NULL);
-	while (a <= index)
+	while (a < index)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 		a++;
 	}
