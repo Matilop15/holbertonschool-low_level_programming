eec07dc6a6a623d9de835c0f2b4ab9d380fc36bf
Matias <3959@holbertonschool.com>
2021-11-15 17:28:34 -0800

    arrglando task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_listint - librerar todo el espacio
+ *@head: pointer to list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *buffer;
+
+	while (head)
+	{
+		buffer = (*head);
+		*head = buffer->next;
+		free(buffer);
+	}
+	*head = NULL;
+}
