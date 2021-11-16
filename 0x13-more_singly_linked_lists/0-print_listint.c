06129e093d7df566deb55e41bb5e0327e4a5cb50
Matias <3959@holbertonschool.com>
2021-11-16 04:15:57 -0800

    arreglando betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,10 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		printf("%i\n", h->n);
+		if (h->next == NULL)
+			printf("(nil)");
+		else
+			printf("%i\n", h->n);
 		h = h->next;
 		size++;
 	}
