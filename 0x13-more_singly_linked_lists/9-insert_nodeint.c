a4f5ecc0137cf3975c4b5af331db412f963e00f7
Matias <3959@holbertonschool.com>
2021-11-16 06:55:08 -0800

    if nexnode es null

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,22 +8,22 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newNode, *temp;
+	listint_t *newnode, *temp;
 	unsigned int a = 0;
 
-	newNode = malloc(sizeof(listint_t));
-	if (newNode == NULL)
-	{
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
 		return (NULL);
-	}
 	temp = *head;
 	while (a < idx)
 	{
+		if (temp == NULL)
+			return (NULL);
 		temp = temp->next;
 		a++;
 	}
-	newNode->n = n;
-	newNode->next = temp->next;
-	temp->next = newNode;
-	return (newNode);
+	newnode->n = n;
+	newnode->next = temp->next;
+	temp->next = newnode;
+	return (newnode);
 }
