7ba6b1c206c24364e3b58f12b3a6e79c805a48cf
Matias <3959@holbertonschool.com>
2021-11-16 05:40:50 -0800

    agregando return 0

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,11 +8,11 @@ int pop_listint(listint_t **head)
 {
 	int a = 0;
 
-
 	listint_t *temp = *head;
-	*head = temp->next;
+
 	if (*head == NULL)
 		return (0);
+	*head = temp->next;
 	a = temp->n;
 	free(temp);
 	return (a);
