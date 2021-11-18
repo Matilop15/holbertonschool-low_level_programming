d196d3511a96328f1ec5194f106a0b20910a0e33
Matias <3959@holbertonschool.com>
2021-11-18 12:07:08 -0800

    arreglando betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *
- *
+ *print_binary - converted num to binary
+ *@n: number to converter
  *
 */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-	
+
 	_putchar((n & 1) + '0');
 }
