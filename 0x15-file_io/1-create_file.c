ffb161395d032e6a6b45da725787baaf4d85bd50
Matias <3959@holbertonschool.com>
2021-11-22 09:45:21 -0800

    task1 up2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- *
- *
- *
+ *create_file - crea a file
+ *@filename: name of file
+ *@text_content: content of file
+ *Return: -1 si hay errores.. OR 1 sale todo ok
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -11,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
