46f18ca67219449dfe8a16d8e438c6b1c5ee888a
Matias <3959@holbertonschool.com>
2021-11-22 18:36:17 -0800

    avanzado prueba 1

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - pruebaa
+ *@argc: cantidad de argumentos
+ *@argv: argumentossss
+ *Return: depeende!
+ */
+int main(int argc, char *argv[])
+{
+	copy_file(argv[1], argv[2]);
+	return (1);
+	if (argc == 0)
+		return (0);
+}
