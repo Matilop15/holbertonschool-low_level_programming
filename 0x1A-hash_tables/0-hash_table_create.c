9b73c0b45714cbf724865f940579dcc66ba31ff8
Matias <3959@holbertonschool.com>
2022-01-28 04:29:26 -0800

    fix requeriment

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,26 +7,24 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *n_table = NULL;
-	hash_node_t **n_array = NULL;
+	hash_table_t *n_table;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 
 	n_table = malloc(sizeof(hash_table_t));
 	if (n_table == NULL)
 		return (NULL);
 
-	n_array = malloc(sizeof(hash_node_t *) * size);
-	if (n_array == NULL)
+	n_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (n_table->array == NULL)
 		return (NULL);
 
+
 	for (i = 0; i < size; i++)
-		n_array[i] = NULL;
+		n_table->array[i] = NULL;
 
-	n_table->array = n_array;
 	n_table->size = size;
-
 
 	return (n_table);
 }
