049de9bee5b5699c42068ff14326b37cd716a4df
Matias <3959@holbertonschool.com>
2022-05-03 09:17:46 -0700

    update task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,7 +16,7 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	while (sizee >= min)
+	while (min <= sizee)
 	{
 		temp = min;
 		printf("Search in array: ");
